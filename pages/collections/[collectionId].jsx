ae43968d546e69199fbd47dabc971b5f89e0481c
Tomas <tomasignaciocontreras@hotmail.com>
2022-02-03 17:58:41 -0300

    [collectionId] component -> nftModule, marketplaceModule and fetchCollectionDataÂ

--- pages/collections/[collectionId].jsx
+++ pages/collections/[collectionId].jsx
@@ -1,10 +1,130 @@
+import React, { useState, useEffect, useMemo } from 'react';
 import { useRouter } from 'next/router';
-import React from 'react';
+import Link from 'next/link'
+import { useWeb3 } from '@3rdweb/hooks'
+import { client } from '../../lib/sanityClient'
+import { ThirdwebSDK } from '@3rdweb/sdk'
+import Header from '../../components/Header'
+import { CgWebsite } from 'react-icons/cg';
+import { AiOutlineInstagram, AiOutlineTwitter } from 'react-icons/ai';
+import { HiDotsVertical } from 'react-icons/hi'
+import NFTCard from '../../components/NFTCard';
+
+const style = {
+  bannerImageContainer: `h-[20vh] w-screen overflow-hidden flex justify-center items-center`,
+  bannerImage: `w-full object-cover`,
+  infoContainer: `w-screen px-4`,
+  midRow: `w-full flex justify-center text-white`,
+  endRow: `w-full flex justify-end text-white`,
+  profileImg: `w-40 h-40 object-cover rounded-full border-2 border-[#202225] mt-[-4rem]`,
+  socialIconsContainer: `flex text-3xl mb-[-2rem]`,
+  socialIconsWrapper: `w-44`,
+  socialIconsContent: `flex container justify-between text-[1.4rem] border-2 rounded-lg px-2`,
+  socialIcon: `my-2`,
+  divider: `border-r-2`,
+  title: `text-5xl font-bold mb-4`,
+  createdBy: `text-lg mb-4`,
+  statsContainer: `w-[44vw] flex justify-between py-4 border border-[#151b22] rounded-xl mb-4`,
+  collectionStat: `w-1/4`,
+  statValue: `text-3xl font-bold w-full flex items-center justify-center`,
+  ethLogo: `h-6 mr-2`,
+  statName: `text-lg w-full text-center mt-1`,
+  description: `text-[#8a939b] text-xl w-max-1/4 flex-wrap mt-4`,
+}
 
 const Collection = () => {
-    const router = useRouter()
+  const router = useRouter()
+  const { provider } = useWeb3()
+  const { collectionId } = router.query
+  const [collection, setCollection] = useState({})
+  const [nfts, setNfts] = useState([])
+  const [listings, setListings] = useState([])
+  console.log(provider)
 
-  return <h1>{router.query.collectionId}</h1>;
+
+  const nftModule = useMemo(() => {
+    if (!provider) return
+
+    const sdk = new ThirdwebSDK(
+      provider.getSigner(),
+      'https://eth-rinkeby.alchemyapi.io/v2/R-MLwd-XRZ3WGIqSEwvzqdyi4XXXnv1m'
+    )
+    return sdk.getNFTModule(collectionId)
+
+  }, [provider]);
+  console.log('NFTMODULE >>', nftModule)
+
+  // get all NFTs in the collection
+  useEffect(() => {
+    if (!nftModule) return
+      ; (async () => {
+        const nfts = await nftModule.getAll()
+
+        setNfts(nfts)
+      })()
+  }, [nftModule])
+
+  const marketPlaceModule = useMemo(() => {
+    if (!provider) return
+
+    const sdk = new ThirdwebSDK(
+      provider.getSigner(),
+      'https://eth-rinkeby.alchemyapi.io/v2/R-MLwd-XRZ3WGIqSEwvzqdyi4XXXnv1m'
+    )
+    return sdk.getMarketplaceModule(
+      '0x1B06762bf7e8ffE2e28FdE265B407C6E7B5C6D14'
+    )
+  }, [provider])
+  console.log('MARKETPLACEMODULE >>', marketPlaceModule)
+
+  // get all listings in the collection
+  useEffect(() => {
+    if (!marketPlaceModule) return
+      ; (async () => {
+        setListings(await marketPlaceModule.getAllListings())
+
+      })()
+  }, [marketPlaceModule])
+
+  const fetchCollectionData = async (sanityClient = client) => {
+    const query = `*[_type == "marketItems" && contractAddress == "${collectionId}"]{
+      "imageUrl": profileImage.asset->url,
+      "bannerImageUrl": bannerImage.asset->url,
+      volumeTraded,
+      createdBy,
+      contractAddress,
+      "creator": createdBy->userName,
+      title,
+      floorPrice,
+      "allowOwners": owners[]->,
+      description
+    }`
+
+    const collectionData = await sanityClient.fetch(query)
+
+    console.log('COLLECTIONDATA >>', collectionData)
+
+    // the query return 1 object inside of an array
+    await setCollection(collectionData[0])
+  }
+
+  useEffect(() => {
+    fetchCollectionData()
+  }, [collectionId])
+
+  return (
+    <div className="overflow-hidden">
+      <Header />
+      <div className={style.bannerImageContainer}>
+        <img className={style.bannerImage}
+          src={
+            collection?.bannerImageUrl
+            ? collection.bannerImageUrl
+            : 'https://via.placeholder.com/200'
+          } alt="banner" />
+      </div>
+    </div>
+  )
 };
 
 export default Collection;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Hero from '../components/Hero'
 import { useWeb3 } from '@3rdweb/hooks'
 import { useEffect } from 'react'
 import { client } from '../lib/sanityClient'
+import toast, { Toaster } from 'react-hot-toast'
 
 const style = {
   wrapper: ``,
@@ -15,24 +16,39 @@ const style = {
 export default function Home() {
   const { address, connectWallet } = useWeb3()
 
+  const welcomeUser = (userName: string, toastHandler = toast) => {
+    toastHandler.success(
+      `Welcome back ${userName !== 'Unnamed' ? `${userName}` : ''}!`,
+      {
+        style: {
+          background: '#04111d',
+          color: '#fff',
+        }
+      }
+    )
+  }
+
   useEffect(() => {
     if (!address) return
 
-    ;(async () => {
-      const userDoc = {
-        _type: 'users',
-        _id: address,
-        userName: 'Unnamed',
-        walletAddress: address,
-      }
+      ; (async () => {
+        const userDoc = {
+          _type: 'users',
+          _id: address,
+          userName: 'Unnamed',
+          walletAddress: address,
+        }
 
-      const result = await client.createIfNotExists(userDoc)
-    })
+        const result = await client.createIfNotExists(userDoc)
+
+        welcomeUser(result.userName)
+      })()
   }, [address])
 
 
   return (
     <div className={style.wrapper}>
+      <Toaster position="top-center" reverseOrder={false} />
       {address ? (
         <>
           <Header />
